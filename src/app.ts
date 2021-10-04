584301263c5c660cdcec352db72cd3b6d7dc2a94
William Tristão de Paula <williamtristao@hotmail.com>
2021-10-04 17:27:19 -0300

    Base project implemented

---
+++ src/app.ts
@@ -0,0 +1,36 @@
+import express, { Application } from 'express'
+import morgan from 'morgan'
+import IndexRoute from './routes/index.route';
+
+export class App {
+    app: Application;
+
+    constructor(
+        private port?: number | string
+    ) {
+        this.app = express();
+        this.settings();
+        this.middlewares();
+        this.routes();
+    }
+
+    private settings() {
+        this.app.set('port', this.port || process.env.PORT || 3000);
+    }
+
+    private middlewares() {
+        this.app.use(morgan('dev'));
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({ extended: false }))
+    }
+
+    private routes() {
+        this.app.use(IndexRoute);
+    }
+
+    async listen(): Promise<void> {
+        await this.app.listen(this.app.get('port'));
+        console.log('Server on port', this.app.get('port'));
+    }
+
+}
